88e87d64a2e1f98bcfb96e6075aef3728dd824c5
Edwin-niwd3 <edwin.navarro@laverne.edu>
2025-10-22 19:49:28 -0700

    got rid of useless import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useLocalStorageState } from './hooks/useLocalStorageState';
 import './App.css'
 import { TeamColumn } from './TeamColumnComponent';
